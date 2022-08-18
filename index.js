b3933b3dae8c90e8a516bff2e5bb42a6ecd1f0b7
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-08-18 16:17:03 +0530

    initial setup done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const app = express()
 const mysql = require('mysql')
 const cors = require('cors')
+const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+require("dotenv").config()
+
+const app = express()
 app.use(bodyParser.json())
+app.use(express.json())
 
 const db = mysql.createPool({
 
@@ -226,7 +230,15 @@ app.get("/verifyuser", (req, res) => {
 })
 
 
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/versemain', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ })
+ .then(() => {
+    console.log('Connected to MongoDB')
+}).catch(err => {
+        console.log('Error connecting to MongoDB: ', err.message)
+});
 
 
 const port = process.env.PORT || 3001;
