ed233fb06a85bd87a1c868823f8b1b243adf3543
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2023-06-02 00:31:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post('/addmovie', (req, res) => {
     var movie = req.body.movie
     var id = req.body.uid;
 
-    const query = `INSERT INTO movies(id, Name)VALUES(${movie.id}, ${movie.original_title}) ON DUPLICATE KEY UPDATE id=id;`
+    var query = `INSERT INTO movies(id, Name)VALUES(${movie.id}, ${movie.original_title}) ON DUPLICATE KEY UPDATE id=id;`
     query += `INSERT INTO user_movies(movie_id, user_id)VALUES(${movie.id}, ${id}) ON DUPLICATE KEY UPDATE id=id;`
     db.query(query, (err, results) => {
         if (err) {
