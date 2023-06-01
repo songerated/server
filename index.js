dd39b97953b8960316439213d78fa97888bb7454
aadityaguptaa <rockonman3@gmail.com>
2023-06-01 19:51:49 +0530

    added for movies

--- index.js
+++ index.js
@@ -66,15 +66,16 @@ app.post('/addmovie', (req, res) => {
     var movie = req.body.movie
     var id = req.body.uid;
 
-    const query = `INSERT INTO movies(id, Name)VALUES(${movie.id}, ${movie.original_title}) ON DUPLICATE KEY UPDATE id=id;`
-    query += `INSERT INTO user_movies(movie_id, user_id)VALUES(${movie.id}, ${id}) ON DUPLICATE KEY UPDATE id=id;`
-    db.query(query, (err, results) => {
-        if (err) {
-            res.status(500).send(err)
-        }
+    res.send(movie)
+    // const query = `INSERT INTO movies(id, Name)VALUES(${movie.id}, ${movie.original_title}) ON DUPLICATE KEY UPDATE id=id;`
+    // query += `INSERT INTO user_movies(movie_id, user_id)VALUES(${movie.id}, ${id}) ON DUPLICATE KEY UPDATE id=id;`
+    // db.query(query, (err, results) => {
+    //     if (err) {
+    //         res.status(500).send(err)
+    //     }
         
-        res.send(results)
-    })
+    //     res.send(results)
+    // })
     
 })
 
